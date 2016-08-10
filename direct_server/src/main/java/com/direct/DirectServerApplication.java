5f6479d711aea30ad3fb8da50a54af736db34393
Shkaruba Nikita <snikita@kodeks.ru>
2016-08-10 11:42:19 +0300

    Add basic auth, add property setting in main(), fix json

--- direct_server/src/main/java/com/direct/DirectServerApplication.java
+++ direct_server/src/main/java/com/direct/DirectServerApplication.java
@@ -1,16 +1,25 @@
 package com.direct;
 
 import com.direct.models.XlsCreator;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 
+import javax.xml.bind.DatatypeConverter;
+import java.io.UnsupportedEncodingException;
+
 @SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class) // Don't add /api route
 public class DirectServerApplication {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnsupportedEncodingException {
+	    setJvmProperties();
 		SpringApplication.run(DirectServerApplication.class, args);
+	}
+	private static void setJvmProperties() {
+		System.setProperty("DIRECT_ADMIN_NAME", "developer");
+		System.setProperty("DIRECT_ADMIN_PASSWORD", "pass");
 	}
 
 	@Bean
--- direct_server/src/main/java/com/direct/controllers/CustomerController.java
+++ direct_server/src/main/java/com/direct/controllers/CustomerController.java
@@ -11,7 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.DatatypeConverter;
 import java.io.*;
 import java.util.Date;
 
@@ -25,11 +29,11 @@ public class CustomerController {
     public String login(Model model) {
         User u = new User();
         model.addAttribute("user", u);
+
         return "login";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    @ResponseBody
     public void postLogin(@ModelAttribute(value = "user") User user, HttpServletResponse response) throws IOException {
         if(isAdmin(user)) {
             response.setContentType("application/vnd.ms-excel");
@@ -38,37 +42,45 @@ public class CustomerController {
             HSSFWorkbook workbook = xlsCreator.createXlsWithAllEntities();
             workbook.write(response.getOutputStream());
         } else {
-            response.getOutputStream().write("{ access: denied }".getBytes());
+            response.setStatus(401);
+            response.getOutputStream().write(("redirect:" + "sht.com").getBytes());
         }
-    }
-    private boolean isAdmin(User user) {
-        return user.getName().equals(System.getenv("DIRECT_ADMIN_NAME")) && user.getPassword().equals(System.getenv("DIRECT_ADMIN_PASSWORD"));
     }
 
     @RequestMapping(value = "/checkAccess", method = RequestMethod.POST, headers = {"Content-type=application/json"})
     @ResponseBody
-    public String postCustomer(@RequestBody Customer customer) {
+    public String postCustomer(@RequestHeader("Authorization") String authorizationHeader, @RequestBody Customer customer) {
         // Todo: refactor those 2 lanes
-        if (customer.isValid()) {
+        if (customer.isValid() && isBasicAuthRight(authorizationHeader)) {
             customer.setCreationDate(new Date());
             customerRepository.save(customer);
 
-            return "{ access: true }";
+            return "{ access: 'granted' }";
         } else {
-            return "{ access: false }";
+            return "{ access: 'denied' }";
         }
     }
 
     @RequestMapping(value = "/checkData", method = RequestMethod.POST, headers = {"Content-type=application/json"})
     @ResponseBody
-    public String postData(@RequestBody CustomerData data) {
+    public String postData(@RequestHeader("Authorization") String authorizationHeader, @RequestBody CustomerData data) {
         // Todo: refactor those 2 lanes
-        if (data.isValid()) {
+        if (data.isValid() && isBasicAuthRight(authorizationHeader)) {
             data.setCreationDate(new Date());
             dataRepository.save(data);
-            return "{ access: true }";
+            return "{ access: 'granted' }";
         } else {
-            return "{ access: false }";
+            return "{ access: 'denied' }";
         }
+    }
+
+    private boolean isAdmin(User user) {
+        return user.getName().equals(System.getProperty("DIRECT_ADMIN_NAME")) && user.getPassword().equals(System.getProperty("DIRECT_ADMIN_PASSWORD"));
+    }
+    private boolean isBasicAuthRight(String authorizationHeader) {
+        String decodedToken = (System.getProperty("DIRECT_ADMIN_NAME") + ":" + System.getProperty("DIRECT_ADMIN_PASSWORD"));
+        String encodedToken = DatatypeConverter.printBase64Binary(decodedToken.getBytes());
+
+        return authorizationHeader.equals("Basic " + encodedToken);
     }
 }
