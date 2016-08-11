d36a3a47112f6fe126cfe65f3c481013436c5fec
Shkaruba Nikita <snikita@kodeks.ru>
2016-08-11 12:34:38 +0300

    Fix CustomerController's json output

--- direct_server/src/main/java/com/direct/controllers/CustomerController.java
+++ direct_server/src/main/java/com/direct/controllers/CustomerController.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.bind.DatatypeConverter;
@@ -55,9 +54,9 @@ public class CustomerController {
             customer.setCreationDate(new Date());
             customerRepository.save(customer);
 
-            return "{ access: 'granted' }";
+            return "{ \"access\": \"granted\" }";
         } else {
-            return "{ access: 'denied' }";
+            return "{ \"access\": \"denied\" }";
         }
     }
 
@@ -68,9 +67,9 @@ public class CustomerController {
         if (data.isValid() && isBasicAuthRight(authorizationHeader)) {
             data.setCreationDate(new Date());
             dataRepository.save(data);
-            return "{ access: 'granted' }";
+            return "{ \"access\": \"granted\" }";
         } else {
-            return "{ access: 'denied' }";
+            return "{ \"access\": \"denied\" }";
         }
     }
 
